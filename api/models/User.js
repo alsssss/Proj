e5bcfe2e58b5b512657399ccde616fda2bb72598
alsssss <104897181+alsssss@users.noreply.github.com>
2022-06-22 15:28:12 +0200

    Files update

---
+++ api/models/User.js
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+const UserSchema = new mongoose.Schema({
+    isAdmin: {
+        type: Boolean,
+        default: false,
+    },
+    isChef: {
+        type: Boolean,
+        default: false,
+    },
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minLength: 1,
+        text: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    img: {
+        type: String,
+    },
+    phone: {
+        type: String,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    orders: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Order"
+    }],
+}, { timestamps: true });
+
+export default mongoose.model("User", UserSchema);
